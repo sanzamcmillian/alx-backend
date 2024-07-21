f2ab7b866af23c27bfef05fb0c0ac1b6eaccd2da
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-21 21:47:16 +0200

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,6 +5,6 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple:
     """returns a tuple of size two"""
-    start = (page -  1) * page_size
+    start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
