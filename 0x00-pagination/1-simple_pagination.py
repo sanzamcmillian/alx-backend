f66f0bba3dfc83d54ae92ff9eb30bc0d6aa7fa70
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-21 22:24:33 +0200

    task 1 try

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env pyhton3
+"""Paginate with more data"""
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """gets page from data provided"""
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start: end]
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """returns a tuple of size two"""
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
