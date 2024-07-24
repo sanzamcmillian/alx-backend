4474b1670e892ef7a2e6292b5eb2c00c633a1e17
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-24 20:56:27 +0200

    MRU

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
-"""Least Recently Used Cache"""
+"""Most Recently Used Cache"""
 from base_caching import BaseCaching
 from collections import OrderedDict
 
 
-class LRUCache(BaseCaching):
-    """LRU cache system"""
+class MRUCache(BaseCaching):
+    """MRU Cache system"""
     def __init__(self):
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """method for assignment"""
+        """assignment method"""
         if key is None or item is None:
             return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                lru, _ = self.cache_data.popitem(True)
-                print(f"DISCARD: {lru}")
+                mru, _ = self.cache_data.popitem(False)
+                print(f"DISCARD: {mru}")
             self.cache_data[key] = item
             self.cache_data.move_to_end(key, last=False)
         else:
-            self.cache_data[key] = item
+            self.cache_data[key] =item
 
     def get(self, key):
         """retrieves item by key"""
