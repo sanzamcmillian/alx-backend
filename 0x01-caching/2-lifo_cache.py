4a2f0b04e07c76b0638b671d9c4cc6dd0a7be78b
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-24 16:37:13 +0200

    LIFO

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Last in first out"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFO caching system"""
+    def __init__(self):
+        """inherited method"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """assignment method"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            last, _ = self.cache_data.popitem(True)
+            print(f"DISCARD: {last}")
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """retrieve item by key"""
+        return self.cache_data.get(key, None)
