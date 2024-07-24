e196256dea3a9b11098c5b6817cc72e40c9c09d0
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-24 20:37:57 +0200

    LRU

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Least Recently Used"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """LRU cache system"""
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """method for assignment"""
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                lru, _ = self.cache_data.popitem(True)
+                print(f"DISCARD: {lru}")
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """retrieves item by key"""
+        if key is None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
